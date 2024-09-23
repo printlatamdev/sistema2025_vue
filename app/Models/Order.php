2ef1130aeecd77c4d6db636baa22483efa541f68
unknown <admin@IT1.colordigital.local>
2024-09-23 17:12:43 -0600

    Begin order module

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'contact_id',
+        'delivery_date',
+        'user_id',
+        'status',
+        'computo',
+        'print',
+        'cut',
+        'country_id',
+        'payment',
+        'mod',
+        'project_id',
+        'application_number',
+    ];
+}
---
+++ database/migrations/2024_09_23_165629_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained();
+            $table->foreignId('contact_id')->constrained();
+            $table->date('delivery_date');
+            $table->foreignId('user_id')->constrained();
+            $table->integer('status')->default(0);
+            $table->integer('computo')->default(0);
+            $table->integer('print')->default(0);
+            $table->integer('cut')->default(0);
+            $table->foreignId('country_id')->constrained();
+            $table->float('payment', 8, 2);
+            $table->float('mod', 8, 2);
+            $table->foreignId('project_id')->constrained();
+            $table->string('application_number')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
